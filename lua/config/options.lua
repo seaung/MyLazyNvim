a0a691ceab130ff2bd46be3883a99be358bee1bc
seaung <seaung.c0m7de@protonmail.com>
2025-04-26 22:41:15 +0800

    feat: 懒只添加一些小的配置

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+local opt = vim.opt -- 简写,方便后续使用
 
-vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+opt.spell = false -- 禁止拼写
+opt.wrap = true -- 自动折行
+opt.relativenumber = false -- 禁用相对行号
+opt.list = true -- 显示空白字符
+opt.listchars = { space = "." }
+
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer" -- 开启rust插件功能
